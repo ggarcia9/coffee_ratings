7b830d0233ea5770e8b4c7ca81e3199229f38f41
augustonetto131 <gutaonetto@gmail.com>
2020-08-08 16:38:15 -0300

    adicionei um dashboard ao segundo gráfico utilizando plotly

--- coffee_ratings_script.R
+++ coffee_ratings_script.R
@@ -11,6 +11,7 @@ library(readxl)
 library(transformr)
 library(lubridate)
 library(gganimate)
+library(plotly)
 
 coffee_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
 
@@ -18,6 +19,7 @@ ggplot(data = coffee_ratings) +
   geom_point(mapping = aes(x = balance, y = total_cup_points, color = country_of_origin))
 
 
-ggplot(data = coffee_ratings) + 
-  geom_point(mapping = aes(x = aroma, y = total_cup_points, color = country_of_origin))
+graf_aroma <- ggplot(data = coffee_ratings) + 
+  geom_point(mapping = aes(x = aroma, y = total_cup_points, color = country_of_origin), show.legend = FALSE)
 
+ggplotly(graf_aroma, tooltip = c("country_of_origin", "aroma"))
